cd832bc61e2af2964ec9efca9d0d6f1adcae4298
Losko <lozan_vlashev@abv.bg>
2017-04-10 13:50:33 +0300

    Added file single.php

--- loskotheme/page-blog.php
+++ loskotheme/page-blog.php
@@ -1,91 +1,114 @@
 <?php get_header(); ?>
 
     <div class="container-fluid">
-    <header class="row blog-my">
-        <h1 class="col-sx-12 blog-my-h1">Блог</h1>
-    </header>
     <div class="row">
+        <header class="blog-my col-sx-10 col-sm-10 col-md-10 col-lg-10">
+            <h1 class="blog-my-h1">Блог</h1>
+        </header>
         <div class="col-xs-10">
-
+            <!--PRINT ONLY LAST POST-->
             <?php
 
-            $lastBLog = new WP_Query('type=post&posts_per_page=1');
+                $lastBLog = new WP_Query('type=post&posts_per_page=1');
 
-            if ( $lastBLog->have_posts() ):
+                if ( $lastBLog->have_posts() ):
 
-                while ( $lastBLog->have_posts()): $lastBLog->the_post(); ?>
+                    while ( $lastBLog->have_posts()): $lastBLog->the_post(); ?>
 
-                    <?php get_template_part('post-content', get_post_format()); ?>
+                        <?php get_template_part('post-content', get_post_format()); ?>
 
-                <?php endwhile;
+                    <?php endwhile;
 
-            endif;
+                endif;
 
-            wp_reset_postdata();
+                wp_reset_postdata();
+
+            ?>
+            <!--PRINT ALL POST NOT FIRT ONE-->
+            <?php
+
+                $args = array(
+                    'type' => 'post',
+                    'offset' => 1,
+                );
+
+                $lastBLog = new WP_Query($args);
+
+                if ( $lastBLog->have_posts() ):
+
+                    while ( $lastBLog->have_posts()): $lastBLog->the_post(); ?>
+
+                        <?php get_template_part('post-content', get_post_format()); ?>
+
+                    <?php endwhile;
+
+                endif;
+
+                wp_reset_postdata();
 
             ?>
 
         </div>
-        <!--PRINT SIDE BAR-->
-        <div class="col-xs-12 col-sm-2 sidebar-my">
+
+        <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 sidebar-my">
 
             <?php get_sidebar(); ?>
 
         </div>
 
         <!--PRINT THE OTHER 2 POSTS NOT THE FIRST ONE-->
-        <div class="col-xs-10">
+       <!-- <div class="col-xs-10">
 
             <?php
+/*
+                $args = array(
+                    'type' => 'post',
+                    'posts_per_page' => 2,
+                    'offset' => 1,
+                );
 
-            $args = array(
-                'type' => 'post',
-                'posts_per_page' => 2,
-                'offset' => 1,
-            );
+                $lastBLog = new WP_Query($args);
 
-            $lastBLog = new WP_Query($args);
+                if ( $lastBLog->have_posts() ):
 
-            if ( $lastBLog->have_posts() ):
+                    while ( $lastBLog->have_posts()): $lastBLog->the_post(); */?>
 
-                while ( $lastBLog->have_posts()): $lastBLog->the_post(); ?>
+                        <?php /*get_template_part('post-content', get_post_format()); */?>
 
-                    <?php get_template_part('post-content', get_post_format()); ?>
+                    <?php /*endwhile;
 
-                <?php endwhile;
+                endif;
 
-            endif;
+                wp_reset_postdata();
 
-            wp_reset_postdata();
+            */?>
 
-            ?>
+        </div>-->
 
-        </div>
-
-        <hr>
+        <!--PRINT SIDE BAR-->
 
         <!--PRINT ONLY TUTORIALS-->
-        <div class="col-xs-10">
+        <!--<div class="col-xs-10">
 
             <?php
-
+/*
             $lastBLog = new WP_Query('type=post&posts_per_page=-1&category=tutorials');
 
             if ( $lastBLog->have_posts() ):
 
-                while ( $lastBLog->have_posts()): $lastBLog->the_post(); ?>
+                while ( $lastBLog->have_posts()): $lastBLog->the_post(); */?>
 
-                    <?php get_template_part('post-content', get_post_format()); ?>
+                    <?php /*get_template_part('post-content', get_post_format()); */?>
 
-                <?php endwhile;
+                <?php /*endwhile;
 
             endif;
 
             wp_reset_postdata();
 
-            ?>
+            */?>
 
-        </div>
+        </div>-->
 
 
     </div>
